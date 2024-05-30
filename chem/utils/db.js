e58e2c0096cdea06b8ca22246119629268d7e861
godot777 <bvr2006bvr2006@gmail.com>
2024-05-30 08:44:42 +0200

    add .env

--- chem/utils/db.js
+++ chem/utils/db.js
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: "postgres.vddqfecxdyxrgwgcbwty",
-  host: "aws-0-eu-central-1.pooler.supabase.com",
-  database: "postgres",
-  password: "SQgr9dgM2PwOCxn3",
-  port: 5432,
+  user: process.env.NEXT_PUBLIC_USER_DB,
+  host: process.env.NEXT_PUBLIC_HOST_DB,
+  database: process.env.NEXT_PUBLIC_DATA_DB,
+  password: process.env.NEXT_PUBLIC_PASS_DB,
+  port: process.env.NEXT_PUBLIC_PORT_DB,
 });
 
 export default pool;
